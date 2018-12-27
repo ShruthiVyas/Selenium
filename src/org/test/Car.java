8bc3ec6342e56891dfa8834ddbcd68540d610145
ShruthiVyas <shruthi.p.vyas@gmail.com>
2018-12-27 08:49:24 +0530

    swift method added

--- src/org/test/Car.java
+++ src/org/test/Car.java
@@ -2,12 +2,16 @@ package org.test;
 
 public class Car {
 	
+	public static void swift() {
+		System.out.println("good mileage");
+
+	}
 	public static void innova() {
 		System.out.println("poor mileage");
-
 	}
 	public static void main(String[] args) {
 		innova();
+		swift();
 	}
 
 }
