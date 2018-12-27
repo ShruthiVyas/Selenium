cc956abda137ddf20b4ab58e8d4281172d91401b
ShruthiVyas <shruthi.p.vyas@gmail.com>
2018-12-27 08:41:54 +0530

    new car program

---
+++ src/org/test/Car.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Car {
+	
+	public static void innova() {
+		System.out.println("poor mileage");
+
+	}
+	public static void main(String[] args) {
+		innova();
+	}
+
+}
